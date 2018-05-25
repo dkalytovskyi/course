ec57004c25a39b9d326973a5c54682995b40f733
Denis Kalytovskyi <31854676+dkalytovskyi@users.noreply.github.com>
2018-05-25 10:44:19 +0300

    Add files via upload

---
+++ CourseWork.h
@@ -0,0 +1,27 @@
+#include "stdafx.h"
+#include "string"
+#include "vector"
+using namespace std;
+#ifndef binary_tree_included
+#define binary_tree_included
+class Tree {
+private:
+	struct Node {
+		int data;
+		Node *left;
+		Node *right;
+	};
+	Node *root;
+	int count;
+	void insert(int key, Node *leaf);
+	string inorder(Node *leaf, string str);
+public:
+	Tree();
+	~Tree();
+	string unique();
+	string maxTimes();
+	void insert(int key);
+	string inorder();
+};
+#endif //binary_tree_included
+
